8862266ba3457d74a552f1625d7e1109c92520b8
Levi244 <levirs999@gmail.com>
2025-06-28 09:11:35 -0300

    Primeira linha de comando

---
+++ forca.py
@@ -0,0 +1,12 @@
+
+palavra = "python"
+letras = []
+chances = 7
+ganhou = False
+
+while True:
+
+    if ganhou:
+        print(f"Parabéns, você ganhou. A palavra era : {palavra}")
+    else:
+        print(f"Você perdeu! A palavra era : {palavra}")
